a62f8c4874cf14e30deebd3ce2762c131660cccc
Ihab Shhadat <ishhadat@everconnect.com>
2022-08-02 12:18:45 +0300

    RMB-483

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,19 +2,10 @@ class ProjectsController < ApplicationController
 
   def index
     @projects = helpers.filter(params)
-    respond_to do |format|
-      format.html
-      format.js
-    end
-
   end
 
   def filter_user_by_company
-    if  params[:company].present?
-      @users = User.where(:company_id => params[:company]).order(:last_name)
-    else
-      @users = User.all.order(:last_name)
-    end
+    @users = helpers.filter_user_by_company params[:company]
   end
 
 
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def filter_user_by_company company_id
+    if  company_id.present?
+      @users = User.where(:company_id => company_id).order(:last_name)
+    else
+      @users = User.all.order(:last_name)
+    end
+    @users
+  end
 end

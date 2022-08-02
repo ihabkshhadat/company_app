febe64b3f7edc2b4987b289c6659086d276b40c0
Ihab Shhadat <ishhadat@everconnect.com>
2022-08-02 12:19:14 +0300

    RMB-483

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
-  def filter_user_by_company company_id
-    if  company_id.present?
+  def filter_user_by_company(company_id)
+    if company_id.present?
       @users = User.where(:company_id => company_id).order(:last_name)
     else
       @users = User.all.order(:last_name)

e85038408ad09a4f5ad9b31a6385c59445caa03d
Ihab Shhadat <ishhadat@everconnect.com>
2022-08-07 11:44:25 +0300

    RMB-483

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,9 +1,10 @@
 class Company < ApplicationRecord
+  default_scope {order('id DESC')}
+
   has_many :users, dependent: :destroy
   DAYS = %i[Sunday Monday Tuesday Wednesday Thursday Friday Saturday].to_h { |i| [i, i] }
   serialize :working_days, Array
   validates :business_name, presence: true
-  before_save :create_company
   before_create do
     self.working_days = working_days.reject(&:blank?)
   end
